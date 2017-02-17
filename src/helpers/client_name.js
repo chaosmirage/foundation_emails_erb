2419ff5166ab7e75624a554eadc786b609348e82
Adel Khamatov <adelkhamatov@yandex.ru>
2017-02-17 10:21:16 +0300

    First version

---
+++ src/helpers/client_name.js
@@ -0,0 +1,6 @@
+module.exports = function(options) {
+  // options.fn(this) = Handelbars content between {{#bold}} HERE {{/bold}}
+
+  var client_name = "&lt;&#37;= @params[:client_name] &#37;&gt;";
+  return client_name;
+}
